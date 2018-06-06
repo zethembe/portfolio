1335d8e415f37c9b34226f699b62a205e79c43ab
zethembe <mpumelelodlamuni@gmail.com>
2018-06-06 18:37:54 +0200

    extended from templates and fixed background image issue

--- app/urls.py
+++ app/urls.py
@@ -5,12 +5,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('blog',views.blog ,name= 'blog'), 
-    path('profile',views.profile, name = 'profile'),
+   
     path('about',views.about, name='about'),
     path('post',views.post, name = 'post'),
     path('',views.home, name='home'),
-    path('signup',views.signup,name = 'signup'),
-    path('login',views.login_page, name = 'login')
+    
+    
 
 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render
 from . import forms
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse
 from app.models import Post
-from django.contrib.auth import login, logout, authenticate
-from django.urls import reverse
+
+
 # Create your views here.
 def blog(request):
 
-      model_view = Post.objects.all()
-      
-      return render(request,'app/blog.html',{'model_view':model_view})
 
-def profile(request):
-    return render(request,'app/profile.html')
+    model_view = Post.objects.all()
+    return render(request,'app/blog.html',{'model_view':model_view})
+
+
 
 def about(request):
     return render(request,'app/about.html')
@@ -24,52 +23,16 @@ def post(request):
         return render(request,'app/post.html', {'form':form})
 
     elif request.method == 'POST':
+
         form = forms.PostForm(request.POST)
-        form.save(commit=True)
-        return HttpResponse('Posted')
+        if form.is_valid():
+            form.save(commit=True)
+        
+            return render(request,'app/blog.html')
 
 def home(request):
   
     return render(request,'app/home.html')
 
-def signup(request):
-    if request.method == 'GET':
-        sign = forms.Signup()
-        return render(request,'app/signup.html',{'form':sign})
-
-    elif request.method =='POST':
-
-        sign = forms.Signup(request.POST)
-        if sign.is_valid():
-
-            f = sign.save()
-            f.set_password(f.password)
-            f.save()
-            return HttpResponse('You have succefully signed up!')
-        else:
-            return HttpResponse('Invalid')
 
 
-def login_page(request):
-    if request.method == 'GET':
-        return render(request,'app/login.html')
-
-    elif request.method == 'POST':
-
-        name_username = request.POST.get('name_username')
-        name_password = request.POST.get('name_userpassword')
-
-        fix = authenticate(username = name_username, password=name_password)
-        print(fix)
-
-        if fix is not None:
-            if fix.is_active:
-                login(request,fix)
-                return HttpResponse('hello')
-                
-            return HttpResponse('hi')
-            
-
-        else:
-            return HttpResponse('not fix')
-        
